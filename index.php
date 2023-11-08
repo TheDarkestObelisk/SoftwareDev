935151b0ce680434c227830f599eabe2ea0304d2
John D. Boles <64272973+TheDarkestObelisk@users.noreply.github.com>
2023-11-08 04:39:01 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-body {
   font-family: Arial, Helvetica, sans-serif;
-  background-color: white;
+  background-color: black; /* Change background color to black */
+  color: white; /* Change text color to white */
 }
 
 * {
@@ -16,13 +16,64 @@ body {
   padding: 16px;
   background-color: white;
 }
+
+/* Full-width input fields */
+input[type="text"], input[type="password"] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type="text"]:focus, input[type="password"]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}
 </style>
 </head>
 <body>
 
 <form method="post" action=""> <!-- Add form tag and action attribute -->
+	<label for="username"><b>Username</b></label><br>
   <input type="text" placeholder="Enter Username" name="username" id="username" required>
   <br><br>
+  
+  <label for="psw"><b>Password</b></label><br>
   <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
   <hr>
   <button type="submit" class="registerbtn">Login</button>
